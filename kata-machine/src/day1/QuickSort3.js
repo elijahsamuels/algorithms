0aa621f67db9c7daa0750732f3b74cbd847e0a54
elijahsamuels <elijahsamuels@gmail.com>
2024-02-26 23:29:02 -0700

    minor update

--- kata-machine/src/day1/QuickSort3.js
+++ kata-machine/src/day1/QuickSort3.js
@@ -1,3 +1,8 @@
+/* 
+- average and best-case time complexity of quicksort is O(n log n)
+- worst-case scenario, time complexity can degrade to O(n^2)
+*/
+
 const quickSort = (arr) => {
 
   const partition = (arr, low, high) => {
@@ -6,23 +11,18 @@ const quickSort = (arr) => {
     for (let j = low; j < high; j++) {
       if (arr[j] <= pivot) {
         i++;
-				console.log('arr[i]:', arr[i]);
-				console.log('arr[j]:', arr[j]);
-
-				// Swap arr[i] and arr[j]
+				
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-				console.log('after swap arr:', arr);
       }
     }
 
-    // Swap arr[i+1] and pivot
     let temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
 
-    return i + 1; // Return the partition index
+    return i + 1; // Return the partition index 
   };
 
   const quickSortHelper = (arr, low, high) => {
@@ -34,16 +34,8 @@ const quickSort = (arr) => {
   };
 
   quickSortHelper(arr, 0, arr.length - 1);
-};
+}; 
 
-// Example:
-const array = [3, 0, 2, 5, -1, 2, -11, 4, 1];
+const array = [3, 0, 2, -1, 1];
 quickSort(array);
 console.log(array); 
-
-/* 
-[3, 0, 2, 5, -1, 2, -11, 4, 1];
-[0, 3, 2, 5, -1, 2, -11, 4, 1];
-
-
-*/

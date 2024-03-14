866558a5b2895dbdcfbf67aa19d240df30eed1df
elijahsamuels <elijahsamuels@gmail.com>
2024-03-14 11:41:44 -0600

    updated binary search

--- kata-machine/src/day1/binarySearch.js
+++ kata-machine/src/day1/binarySearch.js
@@ -1,24 +1,26 @@
 const binarySearch = (arr, target) => {
-  let high = arr.length;
+  let high = arr.length - 1 ;
   let low = 0;
-  let middle = Math.floor((low + high) / 2);
-
-  while (low < high) {
-    if (arr[middle] === target) {
-      return middle;
+	
+  while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+      return mid;
     }
-    if (arr[middle] > target) {
-      high = middle;
+    if (arr[mid] > target) {
+      high = mid - 1;;
     }
-    if (arr[middle] < target) {
-      low = middle;
+    if (arr[mid] < target) {
+      low = mid + 1;
     }
-		return false;
   }
+	return 'not found';
 };
 
-const array = [1, 2, 3, 4, 5, 6, 7];
+const array = [-1, 0, 1, 2, 3, 4, 5, 6, 7];
 
 binarySearch(array, 14);
-binarySearch(array, -4);
+binarySearch(array, -1);
 binarySearch(array, 4);
+binarySearch(array, 7);
+binarySearch(array, -20);
